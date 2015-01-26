3c5ec2f98343912c818aa485050e97fe272d4b01
Anas Imtiaz <anasimtiaz@gmail.com>
2015-01-26 16:55:09 +0000

    Initial Commit

---
+++ SwiftSensorTag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftSensorTag
+//
+//  Created by Anas Imtiaz on 26/01/2015.
+//  Copyright (c) 2015 Anas Imtiaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
